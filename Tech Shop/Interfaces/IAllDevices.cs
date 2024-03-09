021db3eed1b3969e4bbbf8411e70a1ec0df170ef
2North <nikita552003@gmail.com>
2024-03-09 20:02:58 +0200

    Added test models, temporary database is replaced by the mocks

---
+++ Tech Shop/Interfaces/IAllDevices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tech_Shop.Models;
+
+namespace Tech_Shop.Interfaces
+{
+    internal interface IAllDevices
+    {
+        IEnumerable<Device> devices { get;  }
+        Device getDeviceByID(int deviceId);
+    }
+}
---
+++ Tech Shop/Interfaces/IDeviceCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tech_Shop.Models;
+
+namespace Tech_Shop.Interfaces
+{
+    internal interface IDeviceCategory
+    {
+        IEnumerable<DeviceCategory> AllCategories { get; }
+    }
+}
